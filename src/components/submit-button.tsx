3f7c24abfa5bdbeef92e2ff0a6751c4d7c53ba3f
Nick Olson <naolson.1819@gmail.com>
2025-01-27 16:41:09 -0600

    removed extras from submit button

--- src/components/submit-button.tsx
+++ src/components/submit-button.tsx
@@ -1,11 +1,8 @@
-import { useFormStatus } from "react-dom";
+'use client'
 
 const SubmitButton = () => {
-  const { pending } = useFormStatus();
-
   return (
-    <button className="btn btn-primary" type="submit" disabled={pending}>
-      {pending && <span className="loading loading-spinner loading-sm"></span>}
+    <button className="btn btn-primary" type="submit">
       Submit
     </button>
   );
